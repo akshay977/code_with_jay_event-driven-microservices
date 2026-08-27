25ad68a1c5f5ae8589c5242be776412174ff09b0
akshay977 <akshayjoshi977@gmail.com>
2026-08-27 22:20:11 +0530

    testing

--- microservices/customer/src/services/customer-service.test.js
+++ microservices/customer/src/services/customer-service.test.js
@@ -1,6 +1,6 @@
 // Which service it is
 describe("Customer Service", () => {
-	// Which function it is
+	// Which function it is testing
 	describe("SignIn", () => {
 		// Which scenario it is
 		test("Validate user inputs", () => {
